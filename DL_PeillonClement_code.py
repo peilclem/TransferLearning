4b6ba834c46274eb8c2c3857562fbb661566327f
DataPyPeil <peillon.clement74@gmail.com>
2025-02-23 20:41:19 +0100

    Create training loop

--- DL_PeillonClement_code.py
+++ DL_PeillonClement_code.py
@@ -179,17 +179,3 @@ x = train_features[0]
 x.shape
 y = model.forward(x)
 print(y.item())
-
-
-# model = models.Sequential()
-# model.add(layers.Conv2D(32,(3,3), activation = 'relu', input_shape = (150,150,3)))
-# model.add(layers.MaxPooling2D((2,2)))
-# model.add(layers.Conv2D(64,(3,3), activation = 'relu'))
-# model.add(layers.MaxPooling2D((2,2)))
-# model.add(layers.Conv2D(128,(3,3), activation = 'relu'))
-# model.add(layers.MaxPooling2D((2,2)))
-# model.add(layers.Conv2D(128,(3,3), activation = 'relu'))
-# model.add(layers.MaxPooling2D((2,2)))
-# model.add(layers.Flatten())
-# model.add(layers.Dense(512,activation = 'relu'))
-# model.add(layers.Dense(1, activation = 'sigmoid'))
